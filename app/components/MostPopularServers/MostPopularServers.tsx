e0d62a7c7b67adf26e03858d25267878c67ac056
zachrynewycz <hrynewyczz@gmail.com>
2023-07-15 23:17:39 -0500

    fixed popular server card styling

--- app/components/MostPopularServers/MostPopularServers.tsx
+++ app/components/MostPopularServers/MostPopularServers.tsx
@@ -6,11 +6,11 @@ const MostPopularServers = async () => {
 
     return (
         <div className="py-32">
-            <h1 className="text-center font-bebas text-6xl text-stone-200">popular servers 📈</h1>
+            <h1 className="text-center font-bebas text-6xl text-stone-200">popular servers</h1>
 
-            <div className="grid grid-cols-4 gap-10 max-w-7xl mx-auto mt-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-10 max-w-7xl mx-auto mt-10">
                 {servers.data?.map((server: any, index: number) => (
-                    <ServerCard data={server} index={index} />
+                    <ServerCard key={index} data={server} index={index} />
                 ))}
             </div>
         </div>
--- app/components/MostPopularServers/ServerCard.tsx
+++ app/components/MostPopularServers/ServerCard.tsx
@@ -24,15 +24,23 @@ const ServerCard = ({ data, index }: IServerCardProps) => {
     };
 
     return (
-        <motion.div {...animationProps} ref={ref} className="bg-[#1f1f1f] px-5 py-10 text-white cursor-pointer">
-            <h1 className="font-bebas font-bold text-5xl text-white mb-5">
+        <motion.div
+            {...animationProps}
+            ref={ref}
+            className="bg-[#1f1f1f] px-7 py-4 text-white cursor-pointer flex flex-col justify-around h-60 border-4 border-neutral-800"
+        >
+            <h1 className="font-bebas font-bold text-5xl text-white">
                 {data.attributes.players} / {data.attributes.maxPlayers}
             </h1>
 
-            <h1 className="font-open_semi">{data.attributes.name}</h1>
-            <h3 className="font-open_semi text-neutral-400 text-sm">
-                Wiped {formatWipeDate(data.attributes.details.rust_last_wipe)}
-            </h3>
+            <h2 className="font-open_semi">{data.attributes.name}</h2>
+
+            <div className="flex items-center gap-2">
+                <img src="/images/icons/refresh-cw.svg" className="w-5" />
+                <h3 className="font-open_semi text-orange-600 text-sm">
+                    {formatWipeDate(data.attributes.details.rust_last_wipe)}
+                </h3>
+            </div>
         </motion.div>
     );
 };
