c7d07b91bb015500c83346230e13875e593aa127
zachrynewycz <hrynewyczz@gmail.com>
2023-07-25 17:44:47 -0500

    removed console logs and hopefull fix for font errors

--- app/utils/updateServerResults.ts
+++ app/utils/updateServerResults.ts
@@ -5,7 +5,6 @@ import { store } from "../redux/store";
 
 export const updateServerResults = async (nextOrPrevPageUrl?: string) => {
     const servers = await getRustServersByFilter(store.getState().filters, nextOrPrevPageUrl);
-    console.log(servers);
 
     if (servers.links.next) store.dispatch(setNextPageUrl(servers.links.next));
     if (servers.links.prev) store.dispatch(setPrevPageUrl(servers.links.prev));
